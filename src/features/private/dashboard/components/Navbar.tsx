970f6eca5c42118689556c86757930e8f6836af6
EdwinTorrado15 <edwintorrado15@gmail.com>
2025-07-19 09:21:35 -0500

    Ajuste navbar

--- src/features/private/dashboard/components/Navbar.tsx
+++ src/features/private/dashboard/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Layout } from "antd";
+import { Avatar, Dropdown, Layout, Menu, Space } from "antd";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   MenuOutlined,
+  LogoutOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
+import Cookies from "universal-cookie";
 
 const { Header } = Layout;
+
+const cookies = new Cookies();
 
 interface NavbarProps {
   collapsed: boolean;
@@ -31,6 +36,19 @@ export const Navbar = ({
 
   const onClick = isMobile ? toggleDrawer : toggleCollapsed;
 
+  const handleLogout = () => {
+    cookies.remove("token", { path: "/" });
+    window.location.href = "/";
+  };
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="logout" icon={<LogoutOutlined />} onClick={handleLogout}>
+        Cerrar sesión
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <Header
       style={{
@@ -38,15 +56,26 @@ export const Navbar = ({
         background: "#fff",
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between", // Esto separa íconos y perfil
         paddingLeft: 16,
+        paddingRight: 24,
       }}
     >
-      {React.createElement(icon, {
-        className: "trigger",
-        onClick,
-        style: { fontSize: 18, cursor: "pointer" },
-      })}
-      <h2 style={{ marginLeft: 16 }}>Dashboard</h2>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {React.createElement(icon, {
+          className: "trigger",
+          onClick,
+          style: { fontSize: 18, cursor: "pointer" },
+        })}
+        <h2 style={{ marginLeft: 16, marginBottom: 0 }}>Dashboard</h2>
+      </div>
+
+      <Dropdown overlay={menu} placement="bottomRight">
+        <Space style={{ cursor: "pointer" }}>
+          <Avatar icon={<UserOutlined />} />
+          <span>Usuario</span>
+        </Space>
+      </Dropdown>
     </Header>
   );
 };
