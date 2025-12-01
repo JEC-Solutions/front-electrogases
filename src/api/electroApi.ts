4f5976f851099d7c1b22d564b09c123c8aa6bca1
JEC-Solutions <jecsolutionsapps@gmail.com>
2025-11-30 20:27:28 -0500

    fix

--- src/api/electroApi.ts
+++ src/api/electroApi.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_PROD
+const baseURL = import.meta.env.VITE_PROD === true
   ? import.meta.env.VITE_URL_API_PROD || "/api"
   : import.meta.env.VITE_URL_API_DEV || "/api";
+
+console.log("🚀 ~ electroApi.ts:7 ~ baseURL:", baseURL);
 
 export const electroApi = axios.create({
   baseURL,
