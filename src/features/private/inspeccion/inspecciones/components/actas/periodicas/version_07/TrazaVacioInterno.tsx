5195db79edc2cb04e65ddf04a14bcde14bd22091
EdwinTorrado15 <edwintorrado15@gmail.com>
2026-02-12 13:29:32 -0500

    style: ajustar ubicacion de unidades en traza vacio interno

--- src/features/private/inspeccion/inspecciones/components/actas/periodicas/version_07/TrazaVacioInterno.tsx
+++ src/features/private/inspeccion/inspecciones/components/actas/periodicas/version_07/TrazaVacioInterno.tsx
@@ -31,12 +31,14 @@ export const TrazaVacioInterno = ({ inspeccion }: Props) => {
       className={`w-full border-l border-r border-b ${borderClass} ${textClass} box-border`}
     >
       <div className="flex w-full h-[45px]">
+        {/* TITULO */}
         <div
           className={`w-[17%] border-r ${borderClass} bg-gray-200 flex items-center justify-center p-1 text-center font-bold`}
         >
           3.4. TRAZABILIDAD DE VACIO INTERNO
         </div>
 
+        {/* EXISTE VACIO */}
         <div className={`w-[8%] border-r ${borderClass} flex flex-col`}>
           <div
             className={`h-[50%] border-b ${borderClass} flex items-center justify-center text-center px-1 leading-none`}
@@ -53,6 +55,7 @@ export const TrazaVacioInterno = ({ inspeccion }: Props) => {
           </div>
         </div>
 
+        {/* DOCUMENTO LEGAL */}
         <div
           className={`w-[21%] border-r ${borderClass} flex flex-col p-[2px]`}
         >
@@ -63,7 +66,7 @@ export const TrazaVacioInterno = ({ inspeccion }: Props) => {
           <div className="flex flex-col justify-end flex-1 gap-[2px]">
             <div className="flex items-end w-full">
               <span className="whitespace-nowrap mr-1">___ N°</span>
-              <div className="border-b border-black flex-1 text-center h-[10px]">
+              <div className="border-b border-black flex-1 text-center h-[10px] text-[8pt] leading-none">
                 {vacio?.id_documento_legal}
               </div>
             </div>
@@ -76,6 +79,7 @@ export const TrazaVacioInterno = ({ inspeccion }: Props) => {
           </div>
         </div>
 
+        {/* USO */}
         <div
           className={`w-[10%] border-r ${borderClass} flex flex-col p-[2px]`}
         >
@@ -90,30 +94,31 @@ export const TrazaVacioInterno = ({ inspeccion }: Props) => {
           </div>
         </div>
 
+        {/* --- CORRECCION 1: AREA EN PLANTA --- */}
         <div
-          className={`w-[8%] border-r ${borderClass} flex flex-col items-center relative`}
+          className={`w-[8%] border-r ${borderClass} flex flex-col items-center justify-center p-[2px]`}
         >
-          <div className="text-center mt-1">Area en planta</div>
-          <div className="flex-1 flex items-end justify-center w-full pb-1">
-            <span className="text-[9pt] font-bold">m²</span>
+          <div className="text-center leading-none text-[6pt]">
+            Area en planta
           </div>
-          <div className="absolute top-1/2 left-0 w-full text-center font-bold text-[9pt]">
-            {vacio?.area_planta}
+          <div className="flex items-center justify-center gap-1 font-bold text-[8pt]">
+            <span>{vacio?.area_planta}</span>
+            <span className="text-[7pt]">m²</span>
           </div>
         </div>
 
+        {/* --- CORRECCION 2: LADO MINIMO --- */}
         <div
-          className={`w-[8%] border-r ${borderClass} flex flex-col items-center relative`}
+          className={`w-[8%] border-r ${borderClass} flex flex-col items-center justify-center p-[2px]`}
         >
-          <div className="text-center mt-1">Lado minimo</div>
-          <div className="flex-1 flex items-end justify-center w-full pb-1">
-            <span className="text-[9pt] font-bold">m</span>
-          </div>
-          <div className="absolute top-1/2 left-0 w-full text-center font-bold text-[9pt]">
-            {vacio?.lado_minimo}
+          <div className="text-center leading-none text-[6pt]">Lado minimo</div>
+          <div className="flex items-center justify-center gap-1 font-bold text-[8pt]">
+            <span>{vacio?.lado_minimo}</span>
+            <span className="text-[7pt]">m</span>
           </div>
         </div>
 
+        {/* PISOS */}
         <div className={`w-[6%] border-r ${borderClass} flex flex-col`}>
           <div className="text-center mt-1 mb-1"># de pisos:</div>
           <div className="flex-1 bg-gray-100 flex items-center justify-center font-bold m-[2px]">
@@ -121,19 +126,26 @@ export const TrazaVacioInterno = ({ inspeccion }: Props) => {
           </div>
         </div>
 
+        {/* --- CORRECCION 3: AREA LIBRE CUBIERTA --- */}
         <div className={`w-[13%] border-r ${borderClass} flex flex-col`}>
           <div className="h-[50%] flex items-center justify-center gap-1">
             <span>Cubierto:</span>
             <span>SI</span> <Box checked={cubierto} />
             <span>NO</span> <Box checked={cubierto === false} />
           </div>
-          <div className="h-[50%] flex items-end px-[2px] pb-[2px]">
-            <span className="whitespace-nowrap mr-1">Area libre cubierta</span>
-            <div className="border-b border-black flex-1 h-[8px]"></div>
-            <span className="ml-[1px]">m²</span>
+          {/* Ajuste de alineación y tamaño de fuente */}
+          <div className="h-[50%] flex items-end px-[2px] pb-[2px] w-full">
+            <span className="whitespace-nowrap mr-1 text-[6pt] mb-[1px]">
+              Area libre cubierta
+            </span>
+            <div className="border-b border-black flex-1 text-center text-[8pt] leading-none mb-[1px]">
+              {vacio?.area_cubierta}
+            </div>
+            <span className="ml-[1px] text-[7pt] mb-[1px]">m²</span>
           </div>
         </div>
 
+        {/* CUMPLE */}
         <div className="flex-1 flex flex-col">
           <div className={`h-[50%] border-b ${borderClass} flex`}>
             <div
